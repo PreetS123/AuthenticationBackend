1f3cfcfc4b85479a3c4e2a25bab2862cd85d0747
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-25 10:06:57 +0530

    mode-change

--- backend/model/Auth.model.js
+++ backend/model/Auth.model.js
@@ -1,8 +1,8 @@
 const mongoose= require('mongoose');
 
 const authSchema=new mongoose.Schema({
-    name:{type:String,required:true},
-    age:{type:Number,required:true},
+    name:{type:String,required:false},
+    age:{type:Number,required:false},
     email:{type:String,required:true},
     password:{type:String,required:true}
 })
