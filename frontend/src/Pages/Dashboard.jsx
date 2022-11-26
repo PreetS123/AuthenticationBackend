915336ce453de962cd4e8becd170cd456c76fce0
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-26 13:15:37 +0530

    dashboard-created

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
 
 export const Dashboard = () => {
   
   return (
-    <div>Dashboard</div>
+    <div>
+      <DasboardDiv>
+           <ImageWrapper src='https://tse3.mm.bing.net/th?id=OIP.IYur7XGIcoWuf2cQTSzziwHaEK&pid=Api&P=0' />
+           <NavLink to='/users' >Go To Users Page</NavLink>
+      </DasboardDiv>
+    </div>
   )
 }
+
+const DasboardDiv=styled.div`
+ width:70%;
+ margin:auto;
+ padding:2%;
+`
+
+const ImageWrapper=styled.img`
+ width:100%;
+ height:300px;
+`
