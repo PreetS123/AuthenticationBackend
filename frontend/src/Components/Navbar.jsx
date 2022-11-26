718016a8eeadfc2b9de4758784090777c85b16f9
PreetS123 <preetbhardwaj517@gmail.com>
2022-11-26 11:58:36 +0530

    navbar-edited

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,74 +1,49 @@
-import React, { useContext } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "../Styles/Navbar.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
-import { AuthContext } from "../Context/AuthContext";
+
 
 export const Navbar = () => {
-  const [show,setShow]= useState(false);
-  const [state]= useContext(AuthContext);
+  const [show, setShow] = useState(false);
   
-
 
   return (
     <div className={styles.main_nav}>
       {/* logo here */}
       <div className={styles.logo}>
-      <NavLink to="/dashboard"  >
-       <img src='https://tse3.mm.bing.net/th?id=OIP.0jl9haemnq0P4eYzzrfNOgHaGw&pid=Api&P=0' alt='LOGO' />
-      </NavLink>
+        <NavLink to="/dashboard">
+          <img
+            src="https://tse3.mm.bing.net/th?id=OIP.0jl9haemnq0P4eYzzrfNOgHaGw&pid=Api&P=0"
+            alt="LOGO"
+          />
+        </NavLink>
       </div>
 
       {/* list of pages here */}
       <div className={styles.middlediv}>
-        <NavLink
-          to="#"
-         
-        >
-          <p>About</p>
-        </NavLink>
-        <NavLink
-          to="#"
-          
-        >
-          <p>Features</p>
-        </NavLink>
-        <NavLink
-          to="/users"
-         
-        >
+        <NavLink to="/users">
           <p>Users</p>
-        </NavLink>
-        <NavLink
-          to="#"
-         
-        >
-          <p>Testimonial</p>
-        </NavLink>
-        <NavLink
-          to="#"
-          
-        >
-          <p>Help</p>
         </NavLink>
       </div>
 
       {/* authentication button starts from here */}
       <div className={styles.btnbx}>
-        <button><NavLink to='/'>Sign In</NavLink></button>
-        
-          <button><NavLink to='/login' >Log In</NavLink></button>
-        
-        
+        <button>
+          <NavLink to="/">Sign In</NavLink>
+        </button>
+
+        <button>
+          <NavLink to="/login">Log In</NavLink>
+        </button>
       </div>
 
-     
       <div className={styles.hamburger_menu}>
-            <button onClick={() => setShow(!show)}>
-              <GiHamburgerMenu />
-            </button>
-          </div>
+        <button onClick={() => setShow(!show)}>
+          <GiHamburgerMenu />
+        </button>
+      </div>
     </div>
   );
 };
